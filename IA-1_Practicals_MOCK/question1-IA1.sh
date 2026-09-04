11390e429f0c420bab002282d688489ed0dee9e2
Akashi001-ind <akash.yadav.kavir@gmail.com>
2026-09-04 17:50:18 +0530

    Add files via upload

---
+++ IA-1_Practicals_MOCK/question1-IA1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+touch file1 file2a
+
+chmod (u+x) file1
+chmod (u+x) file2a
---
+++ IA-1_Practicals_MOCK/question5.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+if [ $# = 4 ]; then
+
+   echo "Argument 1 $1 "
+
+   echo "Argument 2 $2 "
+
+   echo "Argument 3 $3 "
+
+   echo "Argument 4 $4 "
+
+ else
+	echo "Error"	
+	echo "exit 20"
+fi	
+
